78b00d64163e38d34168d6f07060ecb2e1315019
0xshiku <olachico@icloud.com>
2024-11-04 12:21:20 +0000

    feat: gets a user by id by creating endpoint and sql query

---
+++ cmd/api/users.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"sogo/internal/store"
+	"strconv"
+)
+
+func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
+	userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+
+	user, err := app.store.Users.GetByID(ctx, userID)
+	if err != nil {
+		switch err {
+		case store.ErrNotFound:
+			app.notFoundResponse(w, r, err)
+			return
+		default:
+			app.internalServerError(w, r, err)
+			return
+		}
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
